6bfeeba5a35a1c5ad33d67e5c554d8e32b03ea27
Sirui Hua <jdhuasirui@outlook.com>
2023-05-01 17:33:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,17 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from langchain import OpenAI, ConversationChain, LLMChain, PromptTemplate
-from langchain.chains.conversation.memory import ConversationalBufferWindowMemory
+from langchain.chains.conversation.memory import ConversationBufferMemory
+
 
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 #Langchain implementation
 template = """Assistant is a large language model trained by OpenAI.
-
     Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
-
     Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
-
     Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
-
     {history}
     Human: {human_input}
     Assistant:"""
@@ -30,7 +27,7 @@ chatgpt_chain = LLMChain(
     llm=OpenAI(temperature=0), 
     prompt=prompt, 
     verbose=True, 
-    memory=ConversationalBufferWindowMemory(k=2),
+    memory=ConversationBufferMemory(),
 )
 
 #Message handler for Slack
